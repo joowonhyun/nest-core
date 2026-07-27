10c980036b6787fd404037215e4645bf6ec6cd9a
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2026-07-28 00:52:20 +0900

    add: AuthModule 등록 및 전역 AtGuard 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { LoggerMiddleware } from './common/logger.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './common/guards/at.guards';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
